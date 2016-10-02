75dbef1a9fa587339043b2aaf22aff9c947cbeb5
Vladimir Halme <vhalme@gmail.com>
2016-10-02 19:44:49 +0300

    Refactored link query tests

--- src/data/queries/links.test.js
+++ src/data/queries/links.test.js
@@ -6,26 +6,32 @@ const test = tester({
   contentType: 'application/json'
 });
 
-const createLinkMutation = JSON.stringify({
-  query: 'mutation ($link:TransitLinkInput!) {link(link:$link) {from {name,lat,lng}, to {name,lat,lng}}}',
-  variables: { link: { from: 'moscow', to: 'helsinki' } }
-});
+const validLink = { from: 'moscow', to: 'helsinki' };
+const createLink = async (link) => {
+  
+  const query = JSON.stringify({
+    query: 'mutation ($link:TransitLinkInput!) {link(link:$link) {from {name,lat,lng}, to {name,lat,lng}}}',
+    variables: { link }
+  });
+  
+  const response = await test(query);
+  assert(response.success == true);
+  assert(response.status == 200); 
+  assert(response.data.link.from && response.data.link.to);
+  return response;
+
+};
+
  
 describe('data/queries/links', () => {
 
   it('should create new link', async () => {
-        
-    const response = await test(createLinkMutation);
-    
-    assert(response.success == true);
-    assert(response.status == 200); 
-    assert(response.data.link.from && response.data.link.to);
-
+    createLink(validLink); 
   });
 
   it('returns links by locality id', async () => {
         
-    await test(createLinkMutation);
+    createLink(validLink); 
     
     const query = JSON.stringify({
       query: 'query {links(localityId:1) {from {name,lat,lng}, to {name,lat,lng}}}',
@@ -42,7 +48,7 @@ describe('data/queries/links', () => {
   
   it('returns link by id', async () => {
     
-    await test(createLinkMutation);
+    createLink(validLink);
     
     const query = JSON.stringify({
       query: 'query {link(id:1) {from {name,lat,lng}, to {name,lat,lng}}}',
