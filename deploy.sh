1c3db4f9b82969fa59b646500de84cfb039a3a40
Vladimir Halme <vhalme@gmail.com>
2016-10-03 22:32:06 +0300

    Added docker build to travis conf

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "CURRENT BRANCH....."
-echo $TRAVIS_BRANCH
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  cd docker/web-app
+  ./build.sh --no-cache
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+  docker push USER/REPO;
+fi
