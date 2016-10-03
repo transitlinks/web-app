2d554ef840308839b656d17bbbd7355ca6599fde
Vladimir Halme <vhalme@gmail.com>
2016-10-04 00:31:18 +0300

    Added a webhook test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   cd docker/web-app
   ./build.sh --no-cache
-  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-  docker push vhalme/transitlinks;
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker push vhalme/transitlinks
+  curl -X POST http://198.211.124.91:3005/docker
 fi
