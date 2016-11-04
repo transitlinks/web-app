cc09b18ac73bf9a2d78e7c0d5be3e6dfb05d8d48
Vladimir Halme <vhalme@gmail.com>
2016-11-04 12:38:58 +0200

    Changed host in e2e tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -5,7 +5,7 @@ describe('Test Transitlink frontpage', () => {
 	it('title is displayed correctly', () => {
 		
 		browser
-      .url('http://local.transitlinks.net:3000/')
+      .url('http://localhost:3000/')
       .pause(5000);
     
     assert(browser.getTitle().match(/Transitlinks/i));
