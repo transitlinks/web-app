09e81b7bbe63a85aaaf274c8df87a9843558d070
Vladimir Halme <vhalme@gmail.com>
2017-10-16 22:43:59 +0300

    More debug output in tests

--- test/routes/specs/auth.test.js
+++ test/routes/specs/auth.test.js
@@ -12,26 +12,31 @@ describe('routes/auth', () => {
 
   it('should respond to local login call', async () => {
     const response = await fetch(`${APP_URL}/login`);
+    console.log("local response", response);
     assertResponse(response);
   });
 
   it('should respond to fb login call', async () => {
     const response = await fetch(`${APP_URL}/login/fb`);
+    console.log("fb login response", response);
     assertResponse(response);
   });
 
   it('should respond to fb callback', async () => {
     const response = await fetch(`${APP_URL}/login/fb/callback`);
+    console.log("fb callback response", response);
     assertResponse(response);
   });
   
   it('should respond to google login call', async () => {
     const response = await fetch(`${APP_URL}/login/google`);
+    console.log("google login response", response);
     assertResponse(response);
   });
   
   it('should respond to google callback', async () => {
     const response = await fetch(`${APP_URL}/login/google/callback`);
+    console.log("google callback response", response);
     assertResponse(response);
   });
   
