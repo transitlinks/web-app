ae83288532e05eaf59cd5b0fc0bbe4044d4f6aa4
Vladimir Halme <vhalme@gmail.com>
2019-01-15 18:44:58 +0200

    Updated docker files for web-app

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkill node
 touch ./log.txt
-npm start -- --release --test > ./log.txt 2>&1 &
+yarn start -- --release --test > ./log.txt 2>&1 &
 tail -f ./log.txt |
   while IFS= read -r line
   do
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm run test
-npm run e2e
+yarn run test
+yarn run e2e
