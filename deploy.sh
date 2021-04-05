7373f139a855d8591d2afeaa6ee2ae4ba28db1c8
Vladimir Halme <vhalme@gmail.com>
2021-04-05 10:16:12 +0300

    Improved deployment process

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  cd docker/web-app
-  ./build.sh --no-cache
-  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push vhalme/transitlinks
-  curl -X POST http://198.211.124.91:3005/docker
-fi
+git push origin master
+cd docker/web-app
+./build.sh --no-cache
+docker push vhalme/transitlinks:latest
+cd ../..
+ssh root@transitlinks.net "cd scripts; ./deploy.sh; exit;"
